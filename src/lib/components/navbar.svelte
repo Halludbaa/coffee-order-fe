ddd84771428289660419798639249289cda91b0c
Halludbaa <rinfaabdullah@gmail.com>
2025-08-12 10:59:31 +0700

    initial-commit

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,26 @@
+<script>
+	let order_count = $state(0);
+	const add_order = () => {
+		order_count += 1;
+	};
+</script>
+
+<nav class="fixed top-0 flex h-screen w-1/4 flex-col items-stretch bg-white p-4 shadow">
+	<h1 class="text-4xl font-bold">MoreCoffee</h1>
+	<section class="drop-shadow-x mt-8 flex flex-col rounded-2xl p-2">
+		<h3 class="mb-2 text-xl font-semibold">Category</h3>
+		<ul
+			class="flex flex-col gap-1 *:rounded-lg *:p-2 *:hover:-translate-x-1 *:hover:bg-gray-100 *:hover:font-bold *:hover:duration-150"
+		>
+			<li class="-translate-x-1 bg-gray-100 font-bold">
+				<a href="https://youtube.com" aria-label="true">Coffee</a>
+			</li>
+			<li class=" "><a href="https://youtube.com" aria-label="true">Matcha </a></li>
+			<li class=" "><a href="https://youtube.com" aria-label="true">Milk Base</a></li>
+			<li class=" "><a href="https://youtube.com" aria-label="true">Dessert</a></li>
+		</ul>
+	</section>
+	<button class="mt-auto rounded-xl bg-black py-2 text-2xl font-bold text-white" onclick={add_order}
+		>Order ({order_count})</button
+	>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Navbar from '$lib/components/navbar.svelte';
+</script>
+
+<Navbar />
+
+<main class="ml-[25%] h-screen bg-slate-50 px-4 py-2">
+	<section class="grid-cols-3 *:max-h-50 *:max-w-50 *:border">
+		<div></div>
+		<div></div>
+		<div></div>
+	</section>
+</main>
